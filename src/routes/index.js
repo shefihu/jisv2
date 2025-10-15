216fc3cbd2eb477c8228db1989c3fe6c9ddd1da7
raymondjoseph02 <amraymondjoseph@gmail.com>
2025-10-15 17:07:26 +0100

    fix:implement login page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import AuthLayout from "../layout/AuthLayout";
 import { RoutePaths } from "./routePaths";
+import TestPage from "../pages/uiTest";
+import Login from "../pages/auth/login.jsx";
 export const router = createBrowserRouter([
   {
     element: <AuthLayout />,
     children: [
       {
         path: RoutePaths.LOGIN,
-        element: (
-          <div>
-            <p>login goes here </p>
-          </div>
-        ),
+        element: <Login />,
       },
     ],
+  },
+  {
+    path: RoutePaths.ROOT,
+    element: <TestPage />,
   },
 ]);
